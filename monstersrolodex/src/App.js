20ed1f3cd2a90739a4ba221105a2b073a67b2c65
Wes Jonke <wesjonke@gmail.com>
2023-09-26 20:48:49 -0400

    adding dynamic name change functionality with callbacks to log current state

--- monstersrolodex/src/App.js
+++ monstersrolodex/src/App.js
@@ -31,22 +31,59 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+		//local state
+		this.state = {
+			name: { firstName: 'Wes', lastName: 'Jonke' },
+			company: 'Self Employed Boi',
+		};
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<header className='App-header'>
 					<img src={logo} className='App-logo' alt='logo' />
 					<p>
-						Edit <code>src/App.js</code> and save to reload.
+						Hello my name is {this.state.name.firstName}{' '}
+						{this.state.name.lastName}, I work at{' '}
+						{this.state.company}
 					</p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
+					<button
+						onClick={() => {
+							//adding dynamic name change functionality with callbacks to log current state
+							this.state.name.firstName === 'Wes'
+								? this.setState(
+										() => {
+											return {
+												name: {
+													firstName: 'Gunbunny',
+													lastName: 'Redleg',
+												},
+											};
+										},
+										() => {
+											console.log(this.state);
+										}
+								  )
+								: this.setState(
+										() => {
+											return {
+												name: {
+													firstName: 'Wes',
+													lastName: 'Jonke',
+												},
+											};
+										},
+										() => {
+											console.log(this.state);
+										}
+								  );
+						}}
 					>
-						Learn React
-					</a>
+						Change Name
+					</button>
 				</header>
 			</div>
 		);
